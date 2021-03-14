3e93031cdbfc4ae9b0b45de4295d298706fe6685
gauravgaur20 <gaur.5@iitj.ac.in>
2021-03-15 01:16:40 +0530

    firstcommit

---
+++ backend/data.js
@@ -0,0 +1,46 @@
+export default {
+    products: [
+      {
+        _id: '1',
+        name: 'cotton Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 100,
+        brand: 'Adidas',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 6,
+      },
+      {
+        _id: '2',
+        name: 'slim Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 80,
+        brand: ' Nike',
+        rating: 4.2,
+        numReviews: 5
+      },
+      {
+        _id: '3',
+        name: 'Best Pants',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 70,
+        brand: ' Nike',
+        rating: 4.5,
+        numReviews: 5,
+        countInStock: 60,
+      }, {
+        _id: '4',
+        name: 'Best Pants',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 69,
+        brand: ' Nike',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 60,
+      },
+    ]
+  }
